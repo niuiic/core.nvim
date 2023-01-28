617e93ef1fd02e06fdadad277f8f8ee98d4fee78
niuiic <niuiicde@gmail.com>
2023-01-28 23:15:40 +0800

    feat: add text.selection

--- lua/niuiic-core/text.lua
+++ lua/niuiic-core/text.lua
@@ -1,5 +1,6 @@
 local common = require("niuiic-core.common")
 
+--- insert text
 ---@param content string
 ---@param cursor_pos {row: number, col: number})
 local insert = function(content, cursor_pos)
@@ -17,6 +18,23 @@ local insert = function(content, cursor_pos)
 	end
 end
 
+--- get virtual selection
+---@return string
+local selection = function()
+	local s_start = vim.fn.getpos("'<")
+	local s_end = vim.fn.getpos("'>")
+	local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+	lines[1] = string.sub(lines[1], s_start[3], -1)
+	if n_lines == 1 then
+		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+	else
+		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+	end
+	return table.concat(lines, "\n")
+end
+
 return {
 	insert = insert,
+	selection = selection,
 }
