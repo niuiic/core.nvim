b45ea58c846b900a1d5f7dbcf692e8835d03f2a6
niuiic <niuiicde@gmail.com>
2023-01-28 23:16:14 +0800

    test: add test for text.selection

--- plugin/niuiic-core.lua
+++ plugin/niuiic-core.lua
@@ -3,5 +3,5 @@ local core = require("niuiic-core")
 for _, value in ipairs(core) do
 	vim.api.nvim_create_user_command("Core" .. value[1], function()
 		value[2]()
-	end, {})
+	end, value[3] or {})
 end
