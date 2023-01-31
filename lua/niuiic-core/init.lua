5d6f869a34fbd37a855c38b948597c342515db9b
niuiic <niuiicde@gmail.com>
2023-01-31 10:45:24 +0800

    feat: add entry

---
+++ lua/niuiic-core/init.lua
@@ -0,0 +1,13 @@
+local common = require("niuiic-core.common")
+local file = require("niuiic-core.file")
+local job = require("niuiic-core.job")
+local text = require("niuiic-core.text")
+local win = require("niuiic-core.win")
+
+return {
+	common = common,
+	file = file,
+	job = job,
+	text = text,
+	win = win,
+}
