5c26b741c4f7dbbe5ce91575af57f879dd04a9be
niuiic <niuiicde@gmail.com>
2023-01-31 10:56:45 +0800

    test: merge

--- lua/niuiic-core/init.lua
+++ lua/niuiic-core/init.lua
@@ -4,6 +4,8 @@ local job = require("niuiic-core.job")
 local text = require("niuiic-core.text")
 local win = require("niuiic-core.win")
 
+require('niuiic-core.test')
+
 return {
 	common = common,
 	file = file,
