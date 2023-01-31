15e6de2cb3547fe4935106f697dfad1ac0c3db94
niuiic <niuiicde@gmail.com>
2023-01-31 10:34:50 +0800

    refactor(text.insert): update parameters

--- lua/niuiic-core/text.lua
+++ lua/niuiic-core/text.lua
@@ -2,19 +2,19 @@ local common = require("niuiic-core.common")
 
 --- insert text
 ---@param content string
----@param cursor_pos {row: number, col: number})
-local insert = function(content, cursor_pos)
+---@param pos {row: number, col: number})
+local insert = function(content, pos)
 	local lines = common.str_split(content, "\n")
-	local cur_line = vim.api.nvim_buf_get_lines(0, cursor_pos.row - 1, cursor_pos.row, false)[1]
+	local cur_line = vim.api.nvim_buf_get_lines(0, pos.row - 1, pos.row, false)[1]
 	if #lines == 1 then
-		local new_line = cur_line:sub(0, cursor_pos.col + 1) .. lines[1] .. cur_line:sub(cursor_pos.col + 2)
-		vim.api.nvim_buf_set_lines(0, cursor_pos.row - 1, cursor_pos.row, false, { new_line })
+		local new_line = cur_line:sub(0, pos.col + 1) .. lines[1] .. cur_line:sub(pos.col + 2)
+		vim.api.nvim_buf_set_lines(0, pos.row - 1, pos.row, false, { new_line })
 	else
-		local start_line = cur_line:sub(0, cursor_pos.col + 1) .. lines[1]
-		local end_line = lines[#lines] .. cur_line:sub(cursor_pos.col + 2)
+		local start_line = cur_line:sub(0, pos.col + 1) .. lines[1]
+		local end_line = lines[#lines] .. cur_line:sub(pos.col + 2)
 		lines[1] = start_line
 		lines[#lines] = end_line
-		vim.api.nvim_buf_set_lines(0, cursor_pos.row - 1, cursor_pos.row, false, lines)
+		vim.api.nvim_buf_set_lines(0, pos.row - 1, pos.row, false, lines)
 	end
 end
 
