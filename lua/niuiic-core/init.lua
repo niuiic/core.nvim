5333f871b70313a21dfaac038960042a4b552600
niuiic <niuiicde@gmail.com>
2023-03-04 18:29:41 +0800

    fix(init): wrong export

--- lua/niuiic-core/init.lua
+++ lua/niuiic-core/init.lua
@@ -1,13 +1,13 @@
-local common = require("niuiic-core.common")
 local file = require("niuiic-core.file")
 local job = require("niuiic-core.job")
 local text = require("niuiic-core.text")
 local win = require("niuiic-core.win")
+local lua = require("niuiic-core.lua")
 
 return {
-	common = common,
 	file = file,
 	job = job,
 	text = text,
 	win = win,
+	lua = lua,
 }
