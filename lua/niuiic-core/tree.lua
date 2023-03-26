a797a68d8557150c89251b6ccd752933663d1d10
niuiic <niuiicde@gmail.com>
2023-03-26 15:31:59 +0800

    feat(tree): add tree_view

--- lua/niuiic-core/tree.lua
+++ lua/niuiic-core/tree.lua
@@ -1,30 +1,30 @@
----@class TreeNode
+---@class Tree.Node
 ---@field label string
----@field action NodeAction
----@field status NodeStatus
----@field option NodeOption
+---@field action Tree.NodeAction
+---@field status Tree.NodeStatus
+---@field option Tree.NodeOption
 ---@field level number
----@field children TreeNode[] | nil
+---@field children Tree.Node[] | nil
+---@field extend any
 
----@class NodeAction
----@field on_click fun(node: TreeNode) | nil
----@field on_expand fun(node: TreeNode) | nil
----@field on_fold fun(node: TreeNode) | nil
----@field on_hover fun(node: TreeNode) | nil
+---@class Tree.NodeAction
+---@field on_click fun(node: Tree.Node) | nil
+---@field on_expand fun(node: Tree.Node) | nil
+---@field on_fold fun(node: Tree.Node) | nil
 
----@class NodeStatus
+---@class Tree.NodeStatus
 ---@field expanded boolean
----@field is_leaf boolean
 
----@class NodeOption
----@field color string | nil
+---@class Tree.NodeOption
+---@field hl string | nil
 ---@field icon string | nil
+---@field icon_hl string | nil
 ---@field disable boolean | nil
 ---@field hide boolean | nil
 
----@class TreeViewLine
+---@class Tree.Line
 ---@field line number
----@field node TreeNode
+---@field node Tree.Node
 
 local win = require("niuiic-core.win")
 local lua = require("niuiic-core.lua")
@@ -32,117 +32,186 @@ local lua = require("niuiic-core.lua")
 local expanded_icon = ""
 local unexpanded_icon = ""
 
----@param node TreeNode
+vim.api.nvim_set_hl(0, "TreeViewPrimary", { fg = "#6D7E96" })
+
+---@param node Tree.Node
+---@return boolean
+local is_leaf = function(node)
+	return not node.children or table.maxn(node.children) == 0
+end
+
+---@param node Tree.Node
 ---@return string
 local node_text = function(node)
 	local indent = ""
 	for _ = 2, node.level, 1 do
 		indent = indent .. "  "
 	end
-	if node.status.is_leaf then
-		return string.format("%s%s %s", indent .. "  ", node.option.icon or "", node.label)
+	if is_leaf(node) then
+		return string.format("%s%s %s %s", indent, " ", node.option.icon or " ", node.label)
 	else
 		if node.status.expanded then
-			return string.format("%s%s %s %s", indent, expanded_icon, node.option.icon or "", node.label)
+			return string.format("%s%s %s %s", indent, expanded_icon, node.option.icon or " ", node.label)
 		else
-			return string.format("%s%s %s %s", indent, unexpanded_icon, node.option.icon or "", node.label)
+			return string.format("%s%s %s %s", indent, unexpanded_icon, node.option.icon or " ", node.label)
 		end
 	end
 end
 
----@param node TreeNode
+---@param node Tree.Node
 ---@param bufnr number
 ---@param cur_line number
 local draw_line = function(node, bufnr, cur_line)
 	vim.api.nvim_buf_set_lines(bufnr, cur_line - 1, cur_line, false, { node_text(node) })
-end
-
-local draw_tree
-local cur_line = 1
----@param nodes TreeNode[]
----@param bufnr number
----@param tree_view TreeViewLine[]
-draw_tree = function(nodes, bufnr, tree_view)
-	for _, node in ipairs(nodes) do
-		draw_line(node, bufnr, cur_line)
-		table.insert(tree_view, {
-			line = cur_line,
-			node = node,
-		})
-		cur_line = cur_line + 1
-		if node.status.expanded and not node.status.is_leaf then
-			draw_tree(node.children, bufnr, tree_view)
-		end
+	if node.option.hl then
+		vim.api.nvim_buf_add_highlight(bufnr, -1, node.option.hl, cur_line - 1, node.level * 2 + 1, -1)
+	end
+	if not is_leaf(node) then
+		vim.api.nvim_buf_add_highlight(bufnr, -1, "TreeViewPrimary", cur_line - 1, node.level * 2, node.level * 2 + 1)
+	end
+	if node.option.icon and node.option.icon_hl then
+		vim.api.nvim_buf_add_highlight(
+			bufnr,
+			-1,
+			node.option.icon_hl,
+			cur_line - 1,
+			node.level * 2 + 2,
+			node.level * 2 + 2 + string.len(node.option.icon)
+		)
 	end
 end
 
----@param nodes TreeNode[]
+local draw_tree_wrapper = function()
+	local draw_tree
+	local cur_line = 1
+	---@param nodes Tree.Node[]
+	---@param bufnr number
+	---@param tree_view {lines: Tree.Line[]}
+	draw_tree = function(nodes, bufnr, tree_view)
+		for _, node in ipairs(nodes) do
+			if not node.option.hide then
+				draw_line(node, bufnr, cur_line)
+				table.insert(tree_view.lines, {
+					line = cur_line,
+					node = node,
+				})
+				cur_line = cur_line + 1
+				if not is_leaf(node) and node.status.expanded then
+					draw_tree(node.children, bufnr, tree_view)
+				end
+			end
+		end
+	end
+	return draw_tree
+end
+
+---@param nodes Tree.Node[]
 ---@param bufnr number
----@param tree_view TreeViewLine[]
+---@param tree_view {lines: Tree.Line[]}
 local refresh_tree_view = function(nodes, bufnr, tree_view)
+	tree_view.lines = {}
+	vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
 	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
-	cur_line = 1
-	draw_tree(nodes, bufnr, tree_view)
+	draw_tree_wrapper()(nodes, bufnr, tree_view)
+	vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
 end
 
 ---@param bufnr number
----@param tree_view TreeViewLine[]
----@param nodes TreeNode[]
+---@param tree_view {lines: Tree.Line[]}
+---@param nodes Tree.Node[]
 local register_actions = function(nodes, bufnr, tree_view)
-	local ns_id = vim.api.nvim_create_namespace("tree_view")
-	vim.on_key(function(key)
-		if not vim.api.nvim_win_get_buf(0) == bufnr then
-			return
-		end
+	vim.keymap.set("n", "l", function()
 		local pos = vim.api.nvim_win_get_cursor(0)
-		if key == "l" then
-			local target = lua.list.find(tree_view, function(v)
-				if v.line == pos[1] then
-					return true
-				end
-				return false
-			end)
-			if target then
-				if not target.node.status.expanded then
-					target.node.status.expanded = true
-					refresh_tree_view(nodes, bufnr, tree_view)
-					vim.api.nvim_win_set_cursor(0, pos)
-				end
+		local target = lua.list.find(tree_view.lines, function(v)
+			if v.line == pos[1] then
+				return true
 			end
-		elseif key == "h" then
-			local target = lua.list.find(tree_view, function(v)
-				if v.line == pos[1] then
-					return true
-				end
-				return false
-			end)
-			if target then
-				if target.node.status.expanded then
-					target.node.status.expanded = false
-					refresh_tree_view(nodes, bufnr, tree_view)
-					vim.api.nvim_win_set_cursor(0, pos)
-				end
+			return false
+		end)
+		if target then
+			if not target.node.status.expanded then
+				target.node.status.expanded = true
+				refresh_tree_view(nodes, bufnr, tree_view)
 			end
 		end
-	end, ns_id)
+		vim.api.nvim_win_set_cursor(0, { pos[1], 0 })
+		if target.node.action.on_expand then
+			target.node.action.on_expand()
+		end
+	end, {
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "h", function()
+		local pos = vim.api.nvim_win_get_cursor(0)
+		local target = lua.list.find(tree_view.lines, function(v)
+			if v.line == pos[1] then
+				return true
+			end
+			return false
+		end)
+		if target then
+			if target.node.status.expanded then
+				target.node.status.expanded = false
+				refresh_tree_view(nodes, bufnr, tree_view)
+			end
+		end
+		vim.api.nvim_win_set_cursor(0, { pos[1], 0 })
+		if target.node.action.on_fold then
+			target.node.action.on_fold()
+		end
+	end, {
+		buffer = bufnr,
+	})
+
+	vim.keymap.set("n", "<CR>", function()
+		local pos = vim.api.nvim_win_get_cursor(0)
+		local target = lua.list.find(tree_view.lines, function(v)
+			if v.line == pos[1] then
+				return true
+			end
+			return false
+		end)
+		if target and not target.node.option.disable and target.node.action.on_click then
+			target.node.action.on_click()
+		end
+	end, {
+		buffer = bufnr,
+	})
 end
 
----@param nodes TreeNode[]
+---@param nodes Tree.Node[]
 ---@param options {direction: 'v' | 'h'; size: number; enter: boolean}
+---@return {winnr: number, bufnr: number, tree_view: {lines : Tree.Line[]}}
 local create_tree_view = function(nodes, options)
-	-- open window
+	-- new window
 	local handle = win.split_win(0, options)
-	vim.api.nvim_buf_set_option(handle.bufnr, "filetype", "tree-view")
 	vim.api.nvim_win_set_option(handle.winnr, "number", false)
 	vim.api.nvim_win_set_option(handle.winnr, "relativenumber", false)
+	vim.api.nvim_win_set_option(handle.winnr, "winfixwidth", true)
+	vim.api.nvim_win_set_option(handle.winnr, "list", false)
+	vim.api.nvim_win_set_option(handle.winnr, "wrap", true)
+	vim.api.nvim_win_set_option(handle.winnr, "linebreak", true)
+	vim.api.nvim_win_set_option(handle.winnr, "breakindent", true)
+	vim.api.nvim_win_set_option(handle.winnr, "showbreak", "      ")
 
-	--- @type TreeViewLine[]
-	local tree_view = {}
-	draw_tree(nodes, handle.bufnr, tree_view)
-
+	-- draw tree view
+	---@type {lines: Tree.Line[]}
+	local tree_view = {
+		lines = {},
+	}
+	draw_tree_wrapper()(nodes, handle.bufnr, tree_view)
+	vim.api.nvim_buf_set_option(handle.bufnr, "modifiable", false)
 	register_actions(nodes, handle.bufnr, tree_view)
+
+	return {
+		winnr = handle.winnr,
+		bufnr = handle.bufnr,
+		tree_view = tree_view,
+	}
 end
 
 return {
 	create_tree_view = create_tree_view,
-    }
+	refresh_tree_view = refresh_tree_view,
+}
